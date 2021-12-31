a73e92382a1ecfa45db9ec68df939fd59a5f0858
basemomar94 <basemomar2012@gmail.com>
2021-12-31 23:03:26 +0200

    co

--- app/src/main/java/com/bassem/clinic_userapp/log/MainActivity.kt
+++ app/src/main/java/com/bassem/clinic_userapp/log/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.bassem.clinic_userapp.log
 
+import android.app.Activity
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -13,9 +16,21 @@ import androidx.navigation.ui.setupWithNavController
 import com.bassem.clinic_userapp.R
 import com.bassem.clinic_userapp.ui.Container
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+
+    override fun onStart() {
+        super.onStart()
+        auth= FirebaseAuth.getInstance()
+        val currentUser= auth.currentUser
+
+        if (currentUser!=null){
+            gotohome()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,13 +43,28 @@ class MainActivity : AppCompatActivity() {
                  login.setAllowClickWhenDisabled(false)
              }
              login.alpha=.5F
-             gotohome()
+             Signin()
          }
     }
 
     fun gotohome(){
         val intent:Intent= Intent(this,Container::class.java)
         startActivity(intent)
+    }
+
+    fun Signin(){
+        auth.signInWithEmailAndPassword(mail_log.text.toString().trim(),password_log.text.toString().trim()).addOnCompleteListener {
+            task->
+            if (task.isSuccessful){
+                val id=auth.currentUser?.uid
+                var sharedPreferences:SharedPreferences=getSharedPreferences("PREF",Context.MODE_PRIVATE)
+                var editor=sharedPreferences.edit()
+                editor.putString("id",id)
+                editor.commit()
+                gotohome()
+            }
+        }
+
     }
 
 }
--- app/src/main/java/com/bassem/clinic_userapp/ui/profile/Profile.kt
+++ app/src/main/java/com/bassem/clinic_userapp/ui/profile/Profile.kt
@@ -1,7 +1,65 @@
 package com.bassem.clinic_userapp.ui.profile
 
+import android.content.Context
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.bassem.clinic_userapp.R
+import com.bassem.clinic_userapp.databinding.ProfileFragmentBinding
+import com.google.firebase.firestore.FirebaseFirestore
 
-class Profile () :Fragment(R.layout.profile_fragment) {
+class Profile() : Fragment(R.layout.profile_fragment) {
+    var _binding: ProfileFragmentBinding? = null
+    val binding get() = _binding
+    private lateinit var db: FirebaseFirestore
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = ProfileFragmentBinding.inflate(inflater, container, false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        GettingData()
+    }
+
+    fun GettingData() {
+        var sharedPreferences: SharedPreferences =
+            activity!!.getSharedPreferences("PREF", Context.MODE_PRIVATE)
+        val id = sharedPreferences.getString("id", "noo")!!
+        db = FirebaseFirestore.getInstance()
+        db.collection("patiens_info").document(id).addSnapshotListener { value, error ->
+
+            if (error != null) {
+                println("Firebase error ${error.message}")
+            } else {
+                binding!!.fullnameInfo.text = value?.getString("fullname")
+                binding!!.ageInfo.text = value?.getDouble("age")?.toInt().toString()
+                binding!!.jobInfo.text = value?.getString("job")
+                binding!!.complainInfo.text = value?.getString("complain")
+                binding!!.mailInfo.text = value?.getString("mail")
+
+                binding!!.phoneInfo.text = "0${value?.getDouble("phone")?.toInt().toString()}"
+
+                binding!!.notesInfo.text = value?.getString("note")
+                val sex = value?.getString("sex")
+                binding!!.sexInfo.text = sex
+
+
+            }
+        }
+    }
+
+
 }
